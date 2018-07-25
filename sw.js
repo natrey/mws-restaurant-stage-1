168b4853cc72e932d66558609957767c1c2342af
Наталья Андрейченко <n.andreychenko@fun-box.ru>
2018-07-26 00:37:39 +0300

    ADD cache assets on install event

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('cache-static-1').then(cache => {
+      return cache.addAll([
+        '/',
+        'img/1.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
-  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  )
 });
