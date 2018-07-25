fe7840ac2593137d5e65ed7b0d9ee18cb09cad03
Наталья Андрейченко <n.andreychenko@fun-box.ru>
2018-07-25 20:46:14 +0300

    FIX markers inside self not defined error

--- js/main.js
+++ js/main.js
@@ -121,7 +121,10 @@ const resetRestaurants = (restaurants) => {
   ul.innerHTML = '';
 
   // Remove all map markers
-  self.markers.forEach(m => m.setMap(null));
+  if (self.markers && !!self.markers.length) {
+    self.markers.forEach(m => m.setMap(null));
+  }
+
   self.markers = [];
   self.restaurants = restaurants;
 };
